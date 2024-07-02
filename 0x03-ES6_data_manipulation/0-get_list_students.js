32710985e19eb894dba0f7e45a53b030c8f4df20
sara90-boubou <saraboubou4@gmail.com>
2024-07-02 23:19:08 +0000

    done

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,12 @@
-export default function getListStudents() {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+/* eslint-disable linebreak-style */
+const getListStudents = () => {
+  const students = [];
+  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  students.push(student1, student2, student3);
+
   return students;
-}
+};
+
+export default getListStudents;
