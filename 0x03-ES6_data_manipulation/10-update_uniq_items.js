f63b5bdb29d5f995597ea144a5124ec05e46a14c
sara90-boubou <saraboubou4@gmail.com>
2024-07-02 23:33:58 +0000

    done

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,16 @@
-/* eslint-disable linebreak-style */
-const updateUniqueItems = (aMap) => {
-  if (Object.getPrototypeOf(aMap) !== Map.prototype) throw Error('Cannot process');
-
-  aMap.forEach((key, val) => {
-    if (val === 1) {
-      aMap.set(key, 100);
+/**
+ * Changes the quantity of unique grocery items to 100.
+ * @param {Map<String, number>} map - A map of the name of a
+ * grocery and its quantity.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
   });
-
-  return aMap;
-};
-
-export default updateUniqueItems;
+}
