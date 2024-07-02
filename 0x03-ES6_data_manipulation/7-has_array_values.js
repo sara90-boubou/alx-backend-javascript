35b00108cb146b47417c50607c5ba745e206096f
sara90-boubou <saraboubou4@gmail.com>
2024-07-02 23:36:40 +0000

    done

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,10 @@
-/* eslint-disable linebreak-style */
-const hasValuesFromArray = (aSet, anArr) => {
-  const arr = Array.from(aSet);
-
-  return arr.includes(...anArr);
-};
-
-export default hasValuesFromArray;
+/**
+ * Checks if a set contains each element in an array.
+ * @param {Set} set - The collection of unique items.
+ * @param {*} array - The array of items.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
